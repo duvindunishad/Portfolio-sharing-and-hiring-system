4947e518543558b19205e0368b78a08912cb3474
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-13 01:09:49 +0530

    update database connection

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+
 
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req, res)=>{
-    const product = {
-        id: 1,
-        name: 'hair dress',
-        image: 'some url',
-    }
-    res.send(product);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'portfoliosharing-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready..')
+})
+.catch((err)=> {
+    console.log(err);
 })
 
- app.listen(3000, ()=> {
-    console.log(api);
+//Server
+app.listen(3000, ()=>{
+
     console.log('server is running http://localhost:3000');
- }) 
+})
+
