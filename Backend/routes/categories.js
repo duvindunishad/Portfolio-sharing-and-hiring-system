8d722d99d76837937d695a254631b0fdbe2db8ac
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-13 12:10:20 +0530

    delete item/category

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -10,7 +10,7 @@ router.get(`/`, async (req, res) =>{
     } 
     res.send(categoryList);
 })
-
+//add item/category
 router.post('/', async (req,res)=>{
     let category = new Category({
         name: req.body.name,
@@ -23,6 +23,18 @@ router.post('/', async (req,res)=>{
     return res.status(400).send('the category cannot be created!')
 
     res.send(category);
+})
+ //delete item/category
+ router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
 })
 
 module.exports =router;
