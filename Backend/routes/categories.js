7cd6c6c479ef27a836d9fcf8f0454b5461dbf063
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-13 12:22:42 +0530

    update data

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -5,12 +5,24 @@ const router = express.Router();
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
 
+    //get list of iteo/category
     if(!categoryList) {
         res.status(500).json({success: false})
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 })
-//add item/category
+
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    } 
+    res.status(200).send(category);
+})
+
+
+
 router.post('/', async (req,res)=>{
     let category = new Category({
         name: req.body.name,
@@ -24,8 +36,27 @@ router.post('/', async (req,res)=>{
 
     res.send(category);
 })
- //delete item/category
- router.delete('/:id', (req, res)=>{
+
+//update the category/ items
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+
+    res.send(category);
+})
+
+//delete the category/ items
+router.delete('/:id', (req, res)=>{
     Category.findByIdAndRemove(req.params.id).then(category =>{
         if(category) {
             return res.status(200).json({success: true, message: 'the category is deleted!'})
