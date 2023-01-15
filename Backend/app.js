b6bbbc93daa8e75746d53310d7b80ad60dea1e73
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-15 15:48:34 +0530

    eddite the images uploading

--- Backend/app.js
+++ Backend/app.js
@@ -10,12 +10,13 @@ const errorHandler = require('./helpers/error-handler');
 
 
 app.use(cors());
-app.options('*', cors())
+app.options('*', cors());
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(authJwt);
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 
 
@@ -41,10 +42,10 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'portfoliosharing-database'
+    dbName: 'portfolio'
 })
 .then(()=>{
-    console.log('Database Connection is ready..')
+    console.log('Database Connection is ready..');
 })
 .catch((err)=> {
     console.log(err);
@@ -54,5 +55,5 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 app.listen(3000, ()=>{
 
     console.log('server is running http://localhost:3000');
-})
+});
 
