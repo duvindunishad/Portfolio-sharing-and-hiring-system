00258f50aacf429f3dcce2d0717a0a1560b39bc0
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-31 14:26:24 +0530

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
-const ordersRoutes = require('./routes/orders');
+// const ordersRoutes = require('./routes/orders');
 
 
 
@@ -34,7 +34,7 @@ const api = process.env.API_URL;
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
-app.use(`${api}/orders`, ordersRoutes);
+// app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose.set('strictQuery', false);
